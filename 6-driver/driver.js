16d9d7c5486b43bb8865094da3f912775da53325
Alekolo <kirillover07@gmail.com>
2023-05-04 17:48:13 +0300

    HomeWork-3v2

--- 6-driver/driver.js
+++ 6-driver/driver.js
@@ -2,4 +2,4 @@ const hasLicense = true;
 const age = 18;
 const isDrunk = false;
 
-console.log(`Пользователь ${hasLicense && age >= 18 && !isDrunk ? '' : 'не'} может вожить`);
+console.log(`Пользователь ${hasLicense && age >= 18 && !isDrunk ? '' : 'не '}может вожить`);
