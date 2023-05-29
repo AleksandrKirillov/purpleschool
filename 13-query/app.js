9a179ee63c069326f7efb3b1874ad36c715f6d62
Alekolo <kirillover07@gmail.com>
2023-05-29 16:01:13 +0300

    fix2 HW-4

--- 13-query/app.js
+++ 13-query/app.js
@@ -1,18 +1,17 @@
-
-function prepareQuery(obj){
-    let resultString = '';
-    for( const key in obj ){
-        if(resultString !== '') {
-            resultString += '&';
-        };
-        resultString += key + '=' + obj[key];
+function prepareQuery(obj) {
+  let resultString = "";
+  for (const key in obj) {
+    if (resultString !== "") {
+      resultString += "&";
     }
-    return resultString;
+    resultString += key + "=" + obj[key];
+  }
+  return resultString;
 }
 
 const objectTest = {
-    search: 'Вася',
-    take: 10,
-}
+  search: "Вася",
+  take: 10,
+};
 
 console.log(prepareQuery(objectTest));
