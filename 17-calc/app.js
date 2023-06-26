a981a8787f0a82b8035425b07eb12567ac365c30
Alekolo <kirillover07@gmail.com>
2023-06-26 16:20:33 +0300

    fix5 CR-5

--- 17-calc/app.js
+++ 17-calc/app.js
@@ -1,16 +1,13 @@
 "use strict";
 
-// [...document.querySelectorAll(".button")].forEach(function (item) {
-//     item.addEventListener("click", (event) => clickForCalculation(event));
-// });
+[...document.querySelectorAll(".button")].forEach(function (item) {
+    item.addEventListener("click", (event) => clickForCalculation(event));
+});
 
-document.addEventListener("click", (event) => clickForCalculation(event));
+// document.addEventListener("click", (event) => clickForCalculation(event));
 
 function clickForCalculation(event) {
     const elem = event.target;
-    if (elem.className !== "button") {
-        return;
-    }
     const input1 = document.querySelectorAll(".input")[0].value;
     const input2 = document.querySelectorAll(".input")[1].value;
     const sign = elem.value; //document.activeElement.value;
