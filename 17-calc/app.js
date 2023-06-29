fd145d6986a4c85c4626219232e9900c9400173c
Alekolo <kirillover07@gmail.com>
2023-06-29 16:45:28 +0300

    fix7 CR-5

--- 17-calc/app.js
+++ 17-calc/app.js
@@ -5,40 +5,38 @@
 // });
 
 const mainElem = document.querySelector(".main-buttons");
-mainElem.addEventListener("click", (event) => clickForCalculation(event));
+mainElem.addEventListener("click", clickForCalculation);
 
 function clickForCalculation(event) {
     const elem = event.target;
     if (elem.className !== "button") {
         return;
-    };
-    const input1 = document.querySelectorAll(".input")[0].value;
-    const input2 = document.querySelectorAll(".input")[1].value;
+    }
+    const firstValue = document.querySelectorAll(".input")[0].value;
+    const secondValue = document.querySelectorAll(".input")[1].value;
     const sign = elem.value; //document.activeElement.value;
-    const result = getResultCalc(input1, input2, sign);
+    const result = getResultCalc(firstValue, secondValue, sign);
     document.querySelector(".panel").innerText = result;
-    console.log(input1);
-    console.log(input2);
 }
 
-function getResultCalc(value1, value2, sign) {
-    if (value1 === "" || value2 === "") {
+function getResultCalc(firstValue, secondValue, sign) {
+    if (firstValue === "" || secondValue === "") {
         return "Пустые значения! Введите числа!";
     }
 
-    if (isNaN(value1) || isNaN(value2)) {
+    if (isNaN(firstValue) || isNaN(secondValue)) {
         return "Неверный формат значений. Введите числа!";
     }
 
     switch (sign) {
         case "+":
-            return Number(value1) + Number(value2);
+            return Number(firstValue) + Number(secondValue);
         case "-":
-            return value1 - value2;
+            return firstValue - secondValue;
         case "*":
-            return value1 * value2;
+            return firstValue * secondValue;
         case "/":
-            const resDiv = value1 / value2;
+            const resDiv = firstValue / secondValue;
             if (resDiv === Infinity) {
                 return "На ноль делить нельзя";
             }
